a8be796b4357bbd8f48d3be2bfc3077c495be3bd
OctaviJD <octavijdalmau@gmail.com>
2016-03-29 16:51:58 +0200

    Added files via upload

--- Practica1_FSO.py
+++ Practica1_FSO.py
@@ -17,14 +17,9 @@ def ompleLlista():
 			if fnmatch.fnmatch(element,caracter):
 				listE.insert(END,dirpath[len(dirNou):]+'/'+element)
 
-#CORREGIR!!!!!!!!!!!!!!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 def borrar():
-	#listE.delete(0,END)
-	listE.insert(END,'1')
-	listE.insert(END,'2')
-	listE.insert(END,'3')
-	listE.insert(END,'4')
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	listE.delete(0,END)
+	
 
 def borra_noseleccio():					
 	index = listE.curselection()
@@ -37,30 +32,88 @@ def borra_noseleccio():
 	for j in element:
 		listE.insert(END,j)
 
+
 def borra_seleccio():					
 	index = listE.curselection()
 	for i in  reversed(index):
 		listE.delete(i)
 
+
 def select_Tots():
 	listE.select_set(0,END)
 	
+
 def deselec_Tots():
 	listE.selection_clear(0,END)
 
-def copiar_fitxers():
-	dirCop = tkFileDialog.askdirectory(title='Directori on copiar els arxius')
-	print dirCop
+
+def copiar_fitxers():  
+	global dirNou
+	index = listE.curselection()
+	dirDst = tkFileDialog.askdirectory(title='Directori on copiar els arxius')
+	for i in index:
+		dirArchiu = listE.get(i)
+		dirSrc = os.path.join(dirNou+'/',dirArchiu[1:])
+		print dirSrc
+		os.system("cp -p"+' \"'+dirSrc+'\" \"'+dirDst+'\"')
+
+
+def moure_fitxers():
+	global dirNou
+	index = listE.curselection()
+	dirDst = tkFileDialog.askdirectory(title='Directori on moure els arxius')
+	for i in index:
+		dirArchiu = listE.get(i)
+		dirSrc = os.path.join(dirNou+'/',dirArchiu[1:])
+		print dirSrc
+		os.system("mv "+' \"'+dirSrc+'\" \"'+dirDst+'\"')
+
 
 def esborrar_arxius():
+	global finestra2
+
+	index = listE.curselection()
+	for i in index:
+		rm_file=listE.get(i)
+		print rm_file
+		rm_dir = os.path.join(dirNou+'/',rm_file[1:])
+		print rm_dir
+		os.system("rm "+' \"'+rm_dir+'\" ')
+
+	finestra2.quit()
+
+
+def finestra_esborrar():
+	global finestra2
+
 	finestra2=Toplevel(finestra)
 	finestra2.minsize(0,0)
 	finestra2.title('Esborrar Fitxers')
-	ruta = Label(finestra2, width=60, relief=SUNKEN)
-	ruta.pack(side=LEFT)
-	txt = Label(finestra2, text="Vols esborrar els fitxers seleccionats?")
-	txt.pack(side=CENTER)
-	
+
+	ftxt = Label(finestra2, text="Vols esborrar els fitxers seleccionats?")
+	ftxt.pack(fill=BOTH)
+	espai = Label(finestra2, text=" ")
+	espai.pack(fill=BOTH)
+	bSi=Button(finestra2,text='SI',command=esborrar_arxius)
+	bSi.pack(side=RIGHT,anchor=W)
+	bNo=Button(finestra2,text='NO',command=finestra2.quit)
+	bNo.pack(side=RIGHT,anchor=W)
+
+	finestra2.mainloop()
+
+
+def renombrar_fitxers():
+	global dirNou
+	index = listE.curselection()
+	for i in index:
+		dirArchiu = listE.get(i)
+		dirSrc = os.path.join(dirNou+'/',dirArchiu[1:])
+		
+		print "mv "+' \"'+dirSrc+'\" \"'+dirNou+' $echo \"'+dirArchiu[1:]+'\" | tr \'a\' \'A\')\" '
+
+		#os.system("mv "+' \"'+dirArchiu[1:]+'\" | tr \'a\' \'A\'')
+		#os.system("mv "+' \"'+dirSrc+'\" \"'+dirDst+'\"')
+		os.system("mv "+' \"'+dirSrc+'\" \"'+dirNou+"echo "+' \"'+dirArchiu[1:]+'\" | tr \'a\' \'A\'')
 
 
 
@@ -140,11 +193,11 @@ lSEB2=Label(svB,text="Als selecionats:")
 lSEB2.pack(side=LEFT,anchor=W)
 bONSB2=Button(svB,text='Copiar',command=copiar_fitxers)
 bONSB2.pack(side=LEFT)
-bOSB2=Button(svB,text='Moure')#,command=borra_seleccio)
+bOSB2=Button(svB,text='Moure',command=moure_fitxers)
 bOSB2.pack(side=LEFT)
-bONSB2=Button(svB,text='Esborrar',command=esborrar_arxius)
+bONSB2=Button(svB,text='Esborrar',command=finestra_esborrar)
 bONSB2.pack(side=LEFT)
-bOSB2=Button(svB,text='Renombrar')#,command=borra_seleccio)
+bOSB2=Button(svB,text='Renombrar',command=renombrar_fitxers)
 bOSB2.pack(side=LEFT)
 
 
@@ -154,51 +207,5 @@ svC.pack(fill=X,anchor=S)
 bSortir=Button(svC,text='Sortir',command=finestra.quit)
 bSortir.pack(side=BOTTOM,anchor=W)
 
-
-# def copia_seleccioTED():
-# 	index = listE.size()
-# 	i = 0
-# 	while i<index:
-# 		element = listE.get(0)
-# 		listD.insert(END,element)
-# 		listE.delete(0)
-# 		i=i+1
-
-# def copia_seleccioED():
-#     index = listE.curselection()
-#     while index:
-# 		element = listE.get(index[0])
-# 		listD.insert(END,element)
-# 		listE.delete(index[0])
-# 		index = listE.curselection()
-   
-
-# def copia_seleccioDE():
-#     index = listD.curselection()
-#     while index:
-# 		element = listD.get(index[0])
-# 		listE.insert(END,element)
-# 		listD.delete(index[0])
-# 		index = listD.curselection()
-
-# def copia_seleccioTDE():
-# 	index = listD.size()
-# 	i = 0
-# 	while i<index:
-# 		element = listD.get(0)
-# 		listE.insert(END,element)
-# 		listD.delete(0)
-# 		i=i+1
-
-
-# #Funcionalitats del programa
-# def tecla(event):
-#     print "Premut", repr(event.char)
-
-# def ratoli(event):
-#     print "Click en ", event.x, event.y
-
-# finestra.bind("<Key>", tecla)
-# finestra.bind("<Button-1>", ratoli)
 
 finestra.mainloop()
